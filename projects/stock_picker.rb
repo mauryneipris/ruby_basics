1ac64c22c8fb1a855f762ec0f3434c1bc29986fc
Maury <mbneipris@gmail.com>
2021-03-08 16:38:27 -0500

    initial commit for stock_picker.rb

---
+++ projects/stock_picker.rb
@@ -0,0 +1,8 @@
+# Implement a method #stock_picker that takes in an array of stock prices, 
+# one for each hypothetical day. 
+# It should return a pair of days(array indexes of lowest and highest value) representing the best day to buy
+# and the best day to sell. Days start at 0.
+
+def stock_picker.rb(array)
+  #return index of lowest value and highest value
+end
