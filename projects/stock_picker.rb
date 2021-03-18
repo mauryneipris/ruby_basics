8311458b6635711bd32345be51fe9299ecde1a0a
Maury <mbneipris@gmail.com>
2021-03-17 21:20:46 -0400

    removed test print commands and added commentary

--- projects/stock_picker.rb
+++ projects/stock_picker.rb
@@ -10,14 +10,18 @@ def stock_picker(array)
   array.each_with_index do |sell_price, sell_index|
     array.each_with_index do |buy_price, buy_index|
       if buy_index > sell_index
-      p  profits[[sell_index,buy_index]] = buy_price - sell_price
+      # profits hash key holds array of buy and sell days(respective indexes) and values of the current profits based on 
+      # current prices being iterated on
+      profits[[sell_index,buy_index]] = buy_price - sell_price
       end
     end
   end
-  p profits = profits.sort_by { |k,v| v}
+  # sort the values from least to greatest
+  profits = profits.sort_by { |k,v| v}
+  # max_profit is set to the last key-value pair in the profits hash
   max_profit = profits[-1]
   p "Buy on day #{max_profit[0].first} and sell on day #{max_profit[0].last} for the maximum profit of $#{max_profit[1]}"
-  p max_profit.first
+  max_profit.first
 end
 
 stock_picker(array)
